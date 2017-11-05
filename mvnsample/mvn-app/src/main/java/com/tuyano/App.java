56693769f30eab10584c295a443e61e87e3f44f5
IkarashiToshio <jhypd380@yahoo.co.jp>
2017-11-05 16:10:29 +0900

    mavenの試し実装

---
+++ mvnsample/mvn-app/src/main/java/com/tuyano/App.java
@@ -0,0 +1,23 @@
+package com.tuyano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new App().printMessage("Taro");
+    }
+    
+    public String getMessage(String name) {
+        return "Hi," + name + ". Welcom to Maven World!";
+    }
+    
+    public void printMessage(String name) {
+        System.out.println("\n***** App class *****\n");
+        System.out.println(this.getMessage(name));
+        System.out.println("\n***** App class *****\n");
+    }
+}
