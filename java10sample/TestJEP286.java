9a88f7b83fb13d335bdce978895266e1296a6308
IkarashiToshio <jhypd380@yahoo.co.jp>
2018-01-05 19:06:44 +0900

    JEP286 サンプル実装

---
+++ java10sample/TestJEP286.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class TestJEP286 {
+  public static void main(String[] args) {
+    var list = new ArrayList<String>();
+    list.add("test");
+    System.out.print(list);
+  }
+}
